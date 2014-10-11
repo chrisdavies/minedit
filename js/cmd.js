bbd20abd267fb63a3b079eca7d7356fd2250a69d
Christopher Davies <chris.davies@sparcedge.com>
2014-10-11 11:35:27 -0400

    Adding tab, enter capturing

--- js/cmd.js
+++ js/cmd.js
@@ -1,7 +1,7 @@
 (function () {
     // Autosize textarea directive
     Vue.component('autosize-textarea', {
-        template: '<div class="autosize-textarea"><textarea v-model="val" autocomplete="off"></textarea><pre>{{val}} \n \n</pre></div>',
+        template: '<div class="autosize-textarea"><textarea v-model="val" autocomplete="off"></textarea><pre>{{val}} \n \n</pre></div>'
     });
     
     // Commandline vue controller
@@ -9,6 +9,22 @@
         el: '#cmd-container',
         data: {
             currentCommand: '...'
+        },
+
+        methods: {
+            executeCmd: function (e) {
+                e.preventDefault();
+                document.title = 'Executing';
+            },
+
+            autoCompleteCmd: function (e) {
+                e.preventDefault();
+                document.title = 'Autocompleting';
+            },
+
+            focusCmd: function () {
+                document.getElementsByTagName('textarea').item(0).focus();
+            }
         }
     });
 })();
