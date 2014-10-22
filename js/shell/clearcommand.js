9d510091130ff477424901d0b255236a121f1fd7
Christopher Davies <chris.davies@sparcedge.com>
2014-10-22 17:49:55 -0400

    Moving core commands to their own files

---
+++ js/shell/clearcommand.js
@@ -0,0 +1,11 @@
+﻿// The clear command
+(function (shell) {
+    shell.commands.add({
+        name: 'clear',
+        description: 'Clear the shell terminal',
+
+        execute: function () {
+            shell.stdout.clear();
+        }
+    });
+})(SH.shell);
--- js/shell/helpcommand.js
+++ js/shell/helpcommand.js
@@ -1,7 +1,8 @@
-﻿// Standard shell commands
+﻿// The help command
 (function (shell) {
     var stdout = shell.stdout;
 
+    // help command
     shell.commands.add({
         name: 'help',
         description: 'Display available shell commands',
@@ -15,63 +16,4 @@
             }
         }
     });
-
-    shell.commands.add({
-        name: 'clear',
-        description: 'Clear the shell terminal',
-
-        execute: function () {
-            stdout.clear();
-        }
-    });
-
-    shell.commands.add({
-        name: 'man',
-        description: 'Display usage manual for the specified command',
-        params: [{
-            name: 'command'
-        }],
-
-        execute: function (args) {
-            var cmdName = args.last() || 'man',
-                cmd = shell.commands.get(cmdName);
-
-            if (!cmd) {
-                return { err: 'Could not find command: ' + cmdName };
-            }
-
-            var params = cmd.params || [];
-            cmdParams = params.map(function (p) {
-                var name = p.name;
-                if (!p.required) {
-                    return '[' + name + ']';
-                }
-
-                return name.charAt(0) == '-' ? name : '<' + name + '>';
-            });
-
-            stdout.writeLine();
-            stdout.writeLine('NAME');
-            stdout.writeLine('    ' + cmdName + ' - ' + cmd.description);
-            stdout.writeLine();
-            stdout.writeLine('SYNOPSIS');
-            stdout.writeLine('    ' + cmdName + ' ' + cmdParams.join(' '));
-            stdout.writeLine();
-
-            var options = params.filter(function (p) { return p.description; });
-
-            if (options.length) {
-                stdout.writeLine('OPTIONS');
-                for (var i = 0; i < options.length; ++i) {
-                    var p = options[i];
-                    if (p.description) {
-                        stdout.writeLine('    ' + stdout.pad(p.name, 7) + ' ' + (p.required ? '(required) ' : '') + p.description);
-                    }
-                }
-
-                stdout.writeLine();
-            }
-        }
-    })
-
 })(SH.shell);
